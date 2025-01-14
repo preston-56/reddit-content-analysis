e42e56e51622c5ef0de4cddf528cd1647522ba0d
Preston <prestonosoro56@gmail.com>
2025-01-15 01:32:22 +0300

    chore(settings): update settings for environment variable handling

--- reddit_content_analysis/settings.py
+++ reddit_content_analysis/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(-5g3-#qtbn@%ogz9j76_x+-(uw2un&l(92k9dibuca$^t+7t_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
